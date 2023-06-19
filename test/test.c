1c338da3f0e3f705ab63afcc29a2e8e9fb1af935
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-19 06:28:21 -0700

    implemented the most basic test case for getting block size of the device

--- test/test.c
+++ test/test.c
@@ -2,8 +2,21 @@
 
 #include<block_io.h>
 
+#define ADDITIONAL_FLAGS	0
+#define FILENAME			"test.db"
+
 int main()
 {
-	
+	block_file bf;
+	if(!create_and_open_block_file(&bf, FILENAME, ADDITIONAL_FLAGS) && !open_block_file(&bf, FILENAME, ADDITIONAL_FLAGS | O_TRUNC))
+	{
+		printf("failed to create block file\n");
+		return -1;
+	}
+
+	printf("physical block size of the file %zu\n", get_block_size_for_block_file(&bf));
+
+	close_block_file(&bf);
+
 	return 0;
 }
