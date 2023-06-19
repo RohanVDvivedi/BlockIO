198eabb65cf32dd9ec496eb78d7a21c794f3ecf0
RohanVDvivedi <rohandvivedi@gmail.com>
2023-06-18 23:26:39 -0700

    modified read and write calls to handle partial reads and writes from OS' api

--- src/block_io.c
+++ src/block_io.c
@@ -12,14 +12,34 @@ ssize_t read_blocks_from_block_file(const block_file* fp, void* dest, off_t bloc
 {
 	off_t start_offset = block_id * get_block_size_for_block_file(fp);
 	size_t bytes_count = block_count * get_block_size_for_block_file(fp);
-	return pread(fp->file_descriptor, dest, bytes_count, start_offset);
+
+	ssize_t bytes_read = 0;
+	while(bytes_count > bytes_read)
+	{
+		ssize_t result = pread(fp->file_descriptor, dest + bytes_read, bytes_count - bytes_read, start_offset + bytes_read);
+		if(result < 0)
+			return result;
+		bytes_read += result;
+	}
+
+	return bytes_read;
 }
 
 ssize_t write_blocks_to_block_file(const block_file* fp, const void* src, off_t block_id, size_t block_count)
 {
 	off_t start_offset = block_id * get_block_size_for_block_file(fp);
 	size_t bytes_count = block_count * get_block_size_for_block_file(fp);
-	return pwrite(fp->file_descriptor, src, bytes_count, start_offset);
+
+	ssize_t bytes_written = 0;
+	while(bytes_count > bytes_written)
+	{
+		ssize_t result = pwrite(fp->file_descriptor, src + bytes_written, bytes_count - bytes_written, start_offset + bytes_written);
+		if(result < 0)
+			return result;
+		bytes_written += result;
+	}
+
+	return bytes_written;
 }
 
 int flush_all_writes_to_block_file(const block_file* fp)
